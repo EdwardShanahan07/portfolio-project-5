df5a6386ef79b4ff291290449a48dff3fc87b473
Edward Shanahan <edwardshanahan357@gmail.com>
2023-10-09 18:53:05 +0000

    Refactor profile view queryset ProfilesList view will queryset post_count, followers_count and following_count

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -8,6 +8,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
     
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -39,4 +42,7 @@ class ProfileSerializer(serializers.ModelSerializer):
             'club_team',
             'is_owner',
             'following_id',
+            'posts_count',
+            'followers_count',
+            'following_count',
         ]
