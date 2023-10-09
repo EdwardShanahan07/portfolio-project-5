dd27a815d8e52cf6ac6b2bfb5426305e36506e46
Edward Shanahan <edwardshanahan357@gmail.com>
2023-10-09 18:42:10 +0000

    Add extra fields to post and profile Add like_id field to post serializers Add following_id field to profiles serializers

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,15 +1,28 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 class ProfileSerializer(serializers.ModelSerializer):
     """ Profile Serializer """
     
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
     
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            # print(following)
+            return following.id if following else None
+        return None
 
 
     class Meta:
@@ -25,4 +38,5 @@ class ProfileSerializer(serializers.ModelSerializer):
             'county_team',
             'club_team',
             'is_owner',
+            'following_id',
         ]
