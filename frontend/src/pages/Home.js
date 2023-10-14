2bf172ca867d7a15c5f6c208707b5a50144b1f82
Edward Shanahan <edwardshanahan357@gmail.com>
2023-10-14 16:10:07 +0000

    Add Home text

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import {Row, Col} from "react-bootstrap"
+import styles from "../styles/Home.module.css"
 
 function Home() {
   return (
-    <Row>
-      <Col>
-      Image
+    <Row className={styles.Container}>
+      <Col className={`${styles.Text} text-light text-center p-5`} md={6}>
+      <h1>Welcome To Gaelic Games Network!</h1>
+      <p>Discover, Connect, and Celebrate the Passion for Gaelic Games with Gaelic Games Network - 
+        Your Hub for All Things GAA!
+      </p>
       </Col>
-      <Col>Form</Col>
+      <Col md={6}>
+        Form
+      </Col>
     </Row>
   )
 }
